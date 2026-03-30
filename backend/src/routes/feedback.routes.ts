62aefcd27e2c583c72c1f862516111f0e8455536
Yasanga32 <yasangashanuka33@gmail.com>
2026-03-30 15:17:30 +0530

    feat: add admin auth middleware

--- backend/src/routes/feedback.routes.ts
+++ backend/src/routes/feedback.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { createFeedback, deleteFeedback, getFeedbackById, getFeedbacks, updateFeedback } from "../controllers/feedback.controller.js";
-
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const feedbackRouter = Router();
 
+console.log("FeedbackRouter: authMiddleware is:", typeof authMiddleware);
+
 feedbackRouter.post("/", createFeedback);
-feedbackRouter.get("/", getFeedbacks);
-feedbackRouter.get("/:id", getFeedbackById);
-feedbackRouter.patch("/:id", updateFeedback);
-feedbackRouter.delete("/:id", deleteFeedback);
+feedbackRouter.get("/", authMiddleware,getFeedbacks);
+feedbackRouter.get("/:id", authMiddleware,getFeedbackById);
+feedbackRouter.patch("/:id", authMiddleware, updateFeedback);
+feedbackRouter.delete("/:id", authMiddleware, deleteFeedback);
 
 export default feedbackRouter;
