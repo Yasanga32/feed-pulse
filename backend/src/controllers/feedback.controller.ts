77acd278d8a553c8582459e582b72c244403bfac
Yasanga32 <yasangashanuka33@gmail.com>
2026-03-30 10:10:13 +0530

    feat: complete feedback controller CRUD and add routes

--- backend/src/controllers/feedback.controller.ts
+++ backend/src/controllers/feedback.controller.ts
@@ -29,8 +29,86 @@ export const createFeedback = async (req: Request, res: Response) => {
         });
 
 
+    } catch (error: any) {
+        res.status(500).json({ success: false, message: error.message })
+    }
+}
+
+export const getFeedbacks = async (req: Request, res: Response) => {
+    try {
+        const feedbacks = await Feedback.find();
+        res.status(200).json({
+            success: true,
+            message: "Feedbacks fetched successfully",
+            data: feedbacks
+        });
+    } catch (error: any) {
+        res.status(500).json({ success: false, message: error.message })
+    }
+}
 
 
+export const getFeedbackById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const feedback = await Feedback.findById(id);
+
+        if (!feedback) {
+            return res.status(404).json({
+                success: false,
+                message: "Feedback not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Feedback fetched successfully",
+            data: feedback
+        });
+    } catch (error: any) {
+        res.status(500).json({ success: false, message: error.message })
+    }
+}
+
+export const updateFeedback = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const feedback = await Feedback.findByIdAndUpdate(id, req.body, { new: true });
+
+        if (!feedback) {
+            return res.status(404).json({
+                success: false,
+                message: "Feedback not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Feedback updated successfully",
+            data: feedback
+        });
+    } catch (error: any) {
+        res.status(500).json({ success: false, message: error.message })
+    }
+}
+
+export const deleteFeedback = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const feedback = await Feedback.findByIdAndDelete(id);
+
+        if (!feedback) {
+            return res.status(404).json({
+                success: false,
+                message: "Feedback not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Feedback deleted successfully",
+            data: feedback
+        });
     } catch (error: any) {
         res.status(500).json({ success: false, message: error.message })
     }
--- backend/src/routes/feedback.routes.ts
+++ backend/src/routes/feedback.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { createFeedback } from "../controllers/feedback.controller.js";
+import { createFeedback, deleteFeedback, getFeedbackById, getFeedbacks, updateFeedback } from "../controllers/feedback.controller.js";
 
 
 const feedbackRouter = Router();
 
 feedbackRouter.post("/", createFeedback);
+feedbackRouter.get("/", getFeedbacks);
+feedbackRouter.get("/:id", getFeedbackById);
+feedbackRouter.patch("/:id", updateFeedback);
+feedbackRouter.delete("/:id", deleteFeedback);
 
 export default feedbackRouter;
