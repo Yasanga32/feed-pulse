a339e2b337297e3390eef90ec54aa879c411a04d
Yasanga32 <yasangashanuka33@gmail.com>
2026-04-03 17:26:28 +0530

    fix: update docker compose configuration

--- backend/src/middleware/rateLimit.middleware.ts
+++ backend/src/middleware/rateLimit.middleware.ts
@@ -1,9 +1,9 @@
 import rateLimit from "express-rate-limit";
 
 export const feedbackLimiter = rateLimit({
-  // windowMs: 60 * 60 * 1000, // 1 hour
-  windowMs: 60 * 1000,
-  max: 10, // limit each IP to 5 requests per window
+  windowMs: 60 * 60 * 1000, // 1 hour
+  // windowMs: 60 * 1000,
+  max: 5, // limit each IP to 5 requests per window
   message: {
     success: false,
     message: "Too many submissions. Please try again after 1 hour."
